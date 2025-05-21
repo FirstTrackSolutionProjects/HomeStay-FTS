301caf4eae951d232ffdb81a21234d1bc636f0be
adyaananyamishra <adyaananyamishra@email.com>
2025-05-21 20:50:59 +0530

    Fix case of roomData

--- src/Pages/HotelDetails.jsx
+++ src/Pages/HotelDetails.jsx
@@ -6,7 +6,7 @@ import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import { CheckCircle, Star } from "lucide-react";
-import roomData from "../data/roomdata";
+import roomData from "../data/roomData";
 
 const HotelDetails = () => {
   const { hotelId } = useParams();
